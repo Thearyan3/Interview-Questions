62db751c99a31a972649e9d7e1ffa885405585df
Aryan <chaudharyaryan3456@gmail.com>
2024-12-03 17:24:03 +0530

    updated

--- Day4/IQ.js
+++ Day4/IQ.js
@@ -147,7 +147,19 @@ uniqueColor();
 
 
 
-// Question No. 12 - Given an array of 0's and 1's find out number of 0's
-// Given an array find out total no. of odd and even nos.
+// Question No. 12 - Given an array of 0's and 1's find out number of 0's.
+let arr1 = [0, 1, 1, 0, 0, 0, 1];
+let count = 0;
+for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] == 0){
+        count++;
+    }
+}
+console.log("The number of 0's in given array are :", count);
+
+
+
+// Question No. 13 - Given an array find out total no. of odd and even nos.
+
 // Given a string find out number of vowels 
 // Write a code to create two objects with 2 properties each, one will be string and other will be an array. Create a function to check if all the elements of the arrays in both the objects are same
