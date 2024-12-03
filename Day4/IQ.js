9c17886fcd371fcca134a3bdda99d8155060d3c6
Aryan <chaudharyaryan3456@gmail.com>
2024-12-03 16:45:50 +0530

    updated

---
+++ Day4/IQ.js
@@ -0,0 +1,27 @@
+//Question No. 1 - What are objects in javascript?
+
+// -->In JavaScript, objects are variables that store collections of key-value pairs, which can include properties and methods. They are a core data structure that allows developers to group related data together and split code into logical pieces. 
+// Some characteristics of JavaScript objects are as follows :
+// (i). Properties :- Named values that describe an object's characteristics. For example, a cup's properties might include its color, design, weight, and material. 
+// (ii). Methods :- Functions or actions that an object can perform. For example, a fan's methods might include cooling rooms and controlling humidity. 
+// (iii). Data types :- Objects can contain all other JavaScript data types, such as strings, numbers, and Booleans. 
+
+
+
+// Question No. 2 - What are function constructors?
+
+// -->
+// Give an example of inheritance using function constructor
+// What is the use of setTimeout
+// What are promises and why do we need them?
+// What is the purpose of async/await keywords?
+// What is callback hell
+// What is promise?
+// What are arrow functions?
+// Give an example of async/await
+// What is difference between null and undefined and where to use what?
+// Write code to explain map and filter in arrays
+// Given an array of 0's and 1's find out number of 0's
+// Given an array find out total no. of odd and even nos.
+// Given a string find out number of vowels 
+// Write a code to create two objects with 2 properties each, one will be string and other will be an array. Create a function to check if all the elements of the arrays in both the objects are same
